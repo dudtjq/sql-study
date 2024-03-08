098c4097b1c135a41881584f8636a0dd22f3acdf
dudtjq <dudtjq0415@naver.com>
2024-03-08 18:05:33 +0900

    오라클 sql 커밋

---
+++ 02WHERE절.sql
@@ -0,0 +1,18 @@
+SELECT * FROM employees;
+
+-- WHERE절 비교 (데이터 값은 대/소문자를 구분함)
+SELECT
+    first_name,
+    last_name,
+    job_id
+FROM 
+    employees
+WHERE 
+    job_id = 'IT_PROG';
+    
+SELECT
+    *
+FROM
+    employees
+WHERE
+    last_name = 'King';
